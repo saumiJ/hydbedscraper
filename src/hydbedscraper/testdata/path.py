edf4e1d4b46e553d61c76143b6587fbf340c0ec6
sjoshi <saumitra.joshi@protonmail.com>
2021-04-23 10:07:53 +0200

    add path to new test-data

--- src/hydbedscraper/testdata/path.py
+++ src/hydbedscraper/testdata/path.py
@@ -5,3 +5,4 @@ from pathlib import Path
 _test_dir = Path(__file__).parent.absolute()
 
 test_summary_html = _test_dir.joinpath("summary.html")
+test_government_hospital_data_html = _test_dir.joinpath("government_hospitals_data.html")
