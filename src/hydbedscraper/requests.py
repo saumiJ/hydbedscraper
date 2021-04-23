bccdd5bcd9a778abc6548caf0205018d7d2eaff9
sjoshi <saumitra.joshi@protonmail.com>
2021-04-23 10:21:44 +0200

    add request for private hospitals

--- src/hydbedscraper/requests.py
+++ src/hydbedscraper/requests.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-from hydbedscraper.testdata.path import test_summary_html, test_government_hospital_data_html
+from hydbedscraper.testdata.path import test_summary_html, test_government_hospital_data_html, test_private_hospital_data_html
 from hydbedscraper.types import t_BeautifulSoup, t_Response
 
 base_URL = "http://164.100.112.24/SpringMVC/"
@@ -38,6 +38,22 @@ def get_government_hospitals_page_soup(use_test: bool = True) -> t_BeautifulSoup
             urljoin(base_URL, "getHospital_Beds_Status_Citizen.htm"),
             data={
                 "hospital": "G",
+            },
+            stream=True,
+        )
+        soup = BeautifulSoup(_decode_streamed_response(page), "lxml")
+    return soup
+
+
+def get_private_hospitals_page_soup(use_test: bool = True) -> t_BeautifulSoup:
+    if use_test:
+        with open(test_private_hospital_data_html, "r") as fp:
+            soup = BeautifulSoup(fp, "lxml")
+    else:
+        page = requests.post(
+            urljoin(base_URL, "getHospital_Beds_Status_Citizen.htm"),
+            data={
+                "hospital": "P",
             },
             stream=True,
         )
--- src/hydbedscraper/testdata/path.py
+++ src/hydbedscraper/testdata/path.py
@@ -4,5 +4,6 @@ from pathlib import Path
 
 _test_dir = Path(__file__).parent.absolute()
 
-test_summary_html = _test_dir.joinpath("summary.html")
-test_government_hospital_data_html = _test_dir.joinpath("government_hospitals_data.html")
+test_summary_html = _test_dir.joinpath("data_summary.html")
+test_government_hospital_data_html = _test_dir.joinpath("data_government_hospitals.html")
+test_private_hospital_data_html = _test_dir.joinpath("private_hospitals_data.html")
