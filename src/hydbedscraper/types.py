b15dd01d2f614e020d7b429cba90976e3fc5893d
sjoshi <saumitra.joshi@protonmail.com>
2021-04-25 10:35:08 +0200

    refactor types

--- src/hydbedscraper/types.py
+++ src/hydbedscraper/types.py
@@ -1,15 +1,23 @@
-from bs4 import BeautifulSoup, Tag
-from pandas import DataFrame
-from requests.models import Response
+import bs4
+import camelot
+import pandas
+import requests
+
 from typing import Dict, List
 
-t_BeautifulSoup = BeautifulSoup
-t_Tag = Tag
+# bs4
+t_BeautifulSoup = bs4.BeautifulSoup
+t_Tag = bs4.Tag
 
-t_Response = Response
+# response
+t_Response = requests.models.Response
 
-t_DataFrame = DataFrame
+# pandas
+t_DataFrame = pandas.DataFrame
+t_DataFrameDict = Dict[str, pandas.DataFrame]
+
+# camelot
+t_TableList = camelot.core.TableList
 
 t_SummaryDict = Dict[str, Dict[str, Dict[str, int]]]
 t_FullDict = Dict[str, List]
-t_DataFrameDict = Dict[str, DataFrame]
